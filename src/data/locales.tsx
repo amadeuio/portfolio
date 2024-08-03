13eca0b1b610031bbfe7f35b158f20bc53f63638
Your Name <118369464+amadeuio@users.noreply.github.com>
2024-08-03 17:31:39 +0200

    Update bio

--- src/data/locales.tsx
+++ src/data/locales.tsx
@@ -17,16 +17,17 @@ export const bio = {
   short: {
     en: (
       <p>
-        Self-taught frontend developer from Barcelona with 2 years of programming experience. I
-        strive for simple and reusable code, and excel at coding accurate, responsive and friendly
-        UIs.
+        Self-taught frontend developer from Barcelona with over 2 years of
+        experience specializing in React. I strive for simple and high quality
+        code, and excel at coding accurate, responsive and friendly UIs.
       </p>
     ),
     es: (
       <p>
-        Desarrollador frontend autodidacta de Barcelona con 2 años de experiencia en programación.
-        Busco escribir código simple y reutilizable, y tengo dominio programando interfaces
-        detalladas, responsive y user-friendly.
+        Desarrollador frontend autodidacta de Barcelona con más de 2 años de
+        experiencia especializado en React. Busco escribir código simple y de
+        calidad, y tengo dominio programando interfaces detalladas, responsive y
+        user-friendly.
       </p>
     ),
   },
@@ -34,46 +35,38 @@ export const bio = {
     en: (
       <>
         <p>
-          From jailbreaking my friends iPhones in High School to play Game Boy Pokemon games, to
-          later on studying Physics, I've always had a leaning towards technical related topics.
+          From jailbreaking my friends iPhones in High School to play Game Boy
+          Pokemon games, to later on studying Physics, I've always had a leaning
+          towards technical related topics.
         </p>
         <p>
-          I have spent the last years of my life living abroad, mostly in Central America, doing
-          extensive hospitality work and volunteerings, time that has been very transformative, and
-          where the idea of becoming a developer first arised.
+          I have spent the last years of my life living abroad, mostly in
+          Central America, doing extensive hospitality work and volunteerings,
+          time that has been very transformative, and where the idea of becoming
+          a developer first arised.
         </p>
         <p>
-          Over the last two years, I've been self-teaching my way through the The Odin Project, and
-          recently gotten my first freelance gigs for{" "}
-          <a href="https://bambuda.com" target="_blank" rel="noopener noreferrer">
-            Bambuda
-          </a>
-          . I'm excited to keep expanding my knowledge and skills, hop on a professional development
-          team, and collaborate to create amazing stuff. Let's work together!
+          I'm excited to keep expanding my knowledge and skills, and collaborate
+          to create amazing stuff. Let's work together! 🚀
         </p>
       </>
     ),
     es: (
       <>
         <p>
-          Desde hacer jailbreak a iPhones de mis amigos en secundaria para jugar juegos de Pokémon
-          de Game Boy, hasta más tarde estudiar Física, siempre he tenido una inclinación por temas
-          técnicos.
+          Desde hacer jailbreak a iPhones de mis amigos en la ESO para jugar
+          juegos de Pokémon de Game Boy, hasta más tarde estudiar Física,
+          siempre he tenido una inclinación por temas técnicos.
         </p>
         <p>
-          He pasado los últimos años de mi vida viviendo en el extranjero, principalmente en
-          Centroamérica, realizando sustancial trabajo de hostelería y voluntariados, periodo que ha
-          sido muy transformador y donde surgió por primera vez la idea de convertirme en
-          desarrollador.
+          Me he pasado los últimos años de mi vida viviendo fuera,
+          principalmente en Centroamérica, haciendo trabajos de hostelería y
+          voluntariados, periodo que ha sido muy transformador y donde surgió
+          por primera vez la idea de convertirme en programador.
         </p>
         <p>
-          Durante los últimos dos años, he estado estudiando en The Odin Project, y hace poco
-          conseguí mis primeros trabajos freelance para{" "}
-          <a href="https://bambuda.com" target="_blank" rel="noopener noreferrer">
-            Bambuda
-          </a>
-          . Me motiva seguir ampliando mis conocimientos y habilidades, unirme a un equipo
-          profesional y colaborar para crear cosas increíbles. ¡Trabajemos juntos!
+          Estoy motivado en seguir ampliando mis conocimientos y habilidades, y
+          colaborar para crear cosas geniales. Trabajemos juntos! 🚀
         </p>
       </>
     ),
