37c52af5b7470d1e0dd88bdd61bef6740a7116ab
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-03 17:23:25 +0200

    refactor: migrate App component and dark mode
    - Commented out app scss classes
    - Updated App component to utilize tailwind classes
    - Added dark mode color variables in index.css for tailwind support

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import styles from './App.module.scss';
+import { cn } from '@/utils/cn';
+import { useNightModeContext } from './context/NightModeContext';
 import Contact from './pages/Contact/Contact';
 import Footer from './pages/Footer/Footer';
 import Intro from './pages/Intro/Intro';
 import Navbar from './pages/Navbar/Navbar';
 import Projects from './pages/Projects/Projects';
 import Skills from './pages/Skills/Skills';
-import { useNightModeContext } from './context/NightModeContext';
 
 const App = () => {
   const { isNightMode } = useNightModeContext();
 
   return (
-    <div className={`${styles.app} ${isNightMode ? styles.nightMode : ''}`}>
-      <div className={styles.content}>
+    <div
+      className={cn(
+        'text-primary bg-background box-border flex min-h-screen flex-col items-center overflow-x-hidden px-10 text-base transition-colors duration-200',
+        isNightMode ? 'dark' : '',
+      )}
+    >
+      <div className="flex max-w-5xl flex-col gap-24">
         <Navbar />
         <Intro />
         <Projects />
