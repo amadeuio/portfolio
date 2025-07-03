1d4f150a92d66fe32b4a2b25797643d171a4cb4a
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-03 17:59:18 +0200

    refactor: migrate Navbar component to Tailwind
    - Removed SCSS styles for the Navbar component
    - Updated Navbar component to use Tailwind CSS classes
    - Renamed language toggle function for clarity

--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -1,21 +1,20 @@
 import { useTranslation } from 'react-i18next';
-import styles from './Navbar.module.scss';
 import NightSwitch from './NightSwitch/NightSwitch';
 
 const Navbar = () => {
   const { i18n } = useTranslation();
 
-  const toggleLang = () => {
+  const handleLanguageClick = () => {
     i18n.changeLanguage(i18n.language === 'en' ? 'es' : 'en');
   };
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.left}>
-        <div className={styles.logo}>amadeuio</div>
+    <nav className="mb-5 flex justify-between py-8">
+      <div className="flex items-center">
+        <div className="text-xl font-semibold">amadeuio</div>
       </div>
-      <div className={styles.right}>
-        <div className={styles.toggleLang} onClick={toggleLang}>
+      <div className="flex items-center gap-x-6">
+        <div className="text-tertiary w-[23px] cursor-pointer" onClick={handleLanguageClick}>
           {i18n.language === 'en' ? 'ES' : 'EN'}
         </div>
         <NightSwitch />
