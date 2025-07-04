ee6055ec71fb03f90c3a329fae0c45cb4c539c75
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-04 11:16:02 +0200

    refactor: migrate Projects component to Tailwind
    - Removed SCSS styles for the Projects component
    - Updated Projects component to use Tailwind CSS classes for layout

--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,15 +1,14 @@
 import { useTranslation } from 'react-i18next';
 import projects from '../../data/projects';
 import ProjectCard from './ProjectCard/ProjectCard';
-import styles from './Projects.module.scss';
 
 const Projects = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={styles.projects}>
+    <div className="flex flex-col gap-8">
       <h2>{t('projectsTitle')}</h2>
-      <div className={styles.projectsGallery}>
+      <div className="grid grid-cols-3 gap-8 max-[1125px]:grid-cols-2 max-[738px]:grid-cols-1 max-[738px]:justify-center">
         {projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
