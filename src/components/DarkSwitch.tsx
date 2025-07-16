5bb702f80d9d7c6832a8aed02e0afc694f697289
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-16 15:09:18 +0200

    refactor: update DarkSwitch component to use props for dark mode handling
    - Converted DarkSwitch to accept className, isDarkMode, and onToggle as props
    - Removed dependency on NightModeContext for improved reusability
    - Used implicit return for cleaner and more straight forward code

--- src/components/DarkSwitch.tsx
+++ src/components/DarkSwitch.tsx
@@ -1,28 +1,30 @@
-import { useNightModeContext } from '@/context/NightModeContext';
 import { cn } from '@/utils/cn';
 import { HiMiniMoon, HiMiniSun } from 'react-icons/hi2';
 
-const DarkSwitch = () => {
-  const { isNightMode, toggleNightMode } = useNightModeContext();
+interface DarkSwitchProps {
+  className?: string;
+  isDarkMode: boolean;
+  onToggle: () => void;
+}
 
-  return (
+const DarkSwitch = ({ className, isDarkMode, onToggle }: DarkSwitchProps) => (
+  <div
+    className={cn(
+      className,
+      'relative h-[32px] w-[60px] cursor-pointer rounded-full p-[5px] transition-colors duration-300',
+      isDarkMode ? 'bg-[#121417]' : 'bg-[#eaedef]',
+    )}
+    onClick={onToggle}
+  >
+    <HiMiniSun size={14} className="absolute top-[8px] left-[9px] text-[#c6c8cc]" />
+    <HiMiniMoon size={13} className="absolute top-[9px] right-[9px] text-[#c6c8cc]" />
     <div
       className={cn(
-        'relative h-[32px] w-[60px] cursor-pointer rounded-full p-[5px] transition-colors duration-300',
-        isNightMode ? 'bg-[#121417]' : 'bg-[#eaedef]',
+        'h-[22px] w-[22px] rounded-full shadow-[0_0_3px_rgba(0,0,0,0.4)] transition-all duration-300',
+        isDarkMode ? 'translate-x-[27px] bg-[#1e1f26]' : 'translate-x-0 bg-white',
       )}
-      onClick={toggleNightMode}
-    >
-      <HiMiniSun size={15} className="absolute top-[8px] left-[9px] text-[#c6c8cc]" />
-      <HiMiniMoon size={14} className="absolute top-[8px] right-[9px] text-[#c6c8cc]" />
-      <div
-        className={cn(
-          'h-[22px] w-[22px] rounded-full shadow-[0_0_3px_rgba(0,0,0,0.4)] transition-all duration-300',
-          isNightMode ? 'translate-x-[27px] bg-[#1e1f26]' : 'translate-x-0 bg-white',
-        )}
-      />
-    </div>
-  );
-};
+    />
+  </div>
+);
 
 export default DarkSwitch;
