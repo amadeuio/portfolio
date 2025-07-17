17dbe37b26f80df08b3cb8b6b3b46e6d07b7b002
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2025-07-17 10:40:24 +0200

    refactor: update Contact components to accept the new data structure
    - Modified ContactEmail to accept an email prop instead of hardcoding the email address
    - Refactored ContactLink to accept href and icon as props, simplifying the component structure
    - Updated Contact section to use the contact data for email and links

--- src/components/ContactLink.tsx
+++ src/components/ContactLink.tsx
@@ -1,33 +1,13 @@
 import { Link } from '@/components';
-import { FaLinkedinIn } from 'react-icons/fa';
-import { LuGithub } from 'react-icons/lu';
+import { ContactLink as ContactLinkType } from '@/types';
 
-type ContactKey = 'linkedin' | 'github';
-
-interface ContactLinkProps {
-  contactKey: ContactKey;
-}
-
-const ContactLink = ({ contactKey }: ContactLinkProps) => {
-  const contactConfig: Record<ContactKey, { href: string; icon: React.ReactNode }> = {
-    linkedin: {
-      href: 'https://www.linkedin.com/in/amadeuio',
-      icon: <FaLinkedinIn size={22} className="text-primary" />,
-    },
-    github: {
-      href: 'https://github.com/amadeuio',
-      icon: <LuGithub size={22} className="text-primary" />,
-    },
-  };
-
-  return (
-    <Link
-      href={contactConfig[contactKey].href}
-      className="hover:bg-border cursor-pointer rounded p-3 transition-colors duration-200"
-    >
-      {contactConfig[contactKey].icon}
-    </Link>
-  );
-};
+const ContactLink = ({ href, icon: Icon, iconProps }: ContactLinkType) => (
+  <Link
+    href={href}
+    className="hover:bg-border cursor-pointer rounded p-3 transition-colors duration-200"
+  >
+    <Icon {...iconProps} />
+  </Link>
+);
 
 export default ContactLink;
