a44f127009d682257df720b23b6d7c87b492ca86
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2026-04-10 12:55:50 +0200

    feat: simplify redundant contact and footer texts

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,18 +1,15 @@
-import { ContactEmail, ContactLink, Paragraph, Section } from '@/components';
+import { ContactEmail, ContactLink, Section } from '@/components';
 import { CONTACT, TEXT } from '@/data';
 
-const Contact = () => {
-  return (
-    <Section title={TEXT.contact} childrenClassName="flex flex-col gap-y-6">
-      <Paragraph>{TEXT.contactDescription}</Paragraph>
-      <div className="flex flex-wrap items-center justify-center gap-5 md:justify-start">
-        <ContactEmail email={CONTACT.email} />
-        {CONTACT.links.map((link) => (
-          <ContactLink key={link.href} {...link} />
-        ))}
-      </div>
-    </Section>
-  );
-};
+const Contact = () => (
+  <Section title={TEXT.contact} childrenClassName="flex flex-col gap-y-6">
+    <div className="flex flex-wrap items-center justify-center gap-5 md:justify-start">
+      <ContactEmail email={CONTACT.email} />
+      {CONTACT.links.map((link) => (
+        <ContactLink key={link.href} {...link} />
+      ))}
+    </div>
+  </Section>
+);
 
 export default Contact;
